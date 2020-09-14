acd6e7fd159b70a118358e532bd0cd0d5b7524ba
xaviguasch <xaviguasch@gmail.com>
2020-09-14 21:17:36 +0200

    solved first challenge

--- index.js
+++ index.js
@@ -12,4 +12,29 @@
         - Seasons listed as individual divs
 */
 
-console.log('testing')
+async function findShow(query) {
+  let response = await fetch(
+    `https://api.tvmaze.com/singlesearch/shows?q=${query}&embed=seasons`
+  )
+  let data = await response.json()
+  return data
+}
+
+findShow('office').then((show) => {
+  console.log(show)
+  document.body.innerHTML = `<div class="my-show">
+      <div class="my-show-title">
+          ${show.name}
+      </div>
+      
+      <div class="my-show-summary">
+          ${show.summary}
+      </div>
+      
+      ${show._embedded.seasons
+        .map((season) => {
+          return `<div class="my-show-season">Season ${season.number}</div>`
+        })
+        .join('')}
+  </div>`
+})
