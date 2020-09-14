9fbb56427da8d066a630d38a4f0c886844169c5c
xaviguasch <xaviguasch@gmail.com>
2020-09-14 12:23:07 +0200

    added starter files

---
+++ index.js
@@ -0,0 +1,15 @@
+/* 
+    TV Guide
+    
+    Write an async function called findShow(query)
+        performs a fetch call to:
+        https://api.tvmaze.com/singlesearch/shows?q=${query}&embed=seasons
+        and returns the resulting show object
+        
+    Build a layout to display the show
+        - Title
+        - Summary
+        - Seasons listed as individual divs
+*/
+
+console.log('testing')
